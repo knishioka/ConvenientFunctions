33b96c570a1e48b6936843edadc4cdd0636648da
Kenichiro Nishioka <nishioka@graco.c.u-tokyo.ac.jp>
2012-04-29 15:36:22 +0900

    modified separater

--- python/read_file.py
+++ python/read_file.py
@@ -1,4 +1,4 @@
-def read_tsv_file(file):
+def read_sep_file(file, sep):
     f = open(file)
     for line in f.readlines():
-        id, obj = map(lambda x: int(x), line.rstrip('\n').split('\t'))
+        id, obj = map(lambda x: int(x), line.rstrip('\n').split(sep))
