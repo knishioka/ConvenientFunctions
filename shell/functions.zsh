ec359c8150f9d33c8102d077d6646ed152dd9587
Kenichiro Nishioka <nishioka@3idea.jp>
2019-02-10 13:06:48 +0000

    add date to file and zip file

--- shell/functions.zsh
+++ shell/functions.zsh
@@ -2,3 +2,14 @@
 function kms-decrypt() {
   aws kms decrypt --ciphertext-blob fileb://<(echo "$1"|base64 -d) | jq .Plaintext --raw-output |base64 -d
 }
+
+# ファイルに日付をつける
+function adddate () {
+    mv $1 `date "+%Y%m%d"`_$1
+}
+
+# 暗号化しながらファイルに名前をつける
+function ezip () {
+    zip -re `date "+%Y%m%d"`_$1.zip $1
+}
+
