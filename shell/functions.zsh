3b719f1ebc14fa26ffc02265adffaca733b21513
Kenichiro Nishioka <nishioka@3idea.jp>
2019-06-22 09:38:00 +0900

    added functions to view files on s3

--- shell/functions.zsh
+++ shell/functions.zsh
@@ -13,3 +13,19 @@ function ezip () {
     zip -re `date "+%Y%m%d"`_$1.zip $1
 }
 
+# S3からファイルを取得してそのまま表示
+function sg () {
+    s3cmd get --no-progress $1 -
+}
+
+# S3からファイルを取得してlessで開く
+function sless() {
+    s3cmd get --no-progress $1 - | less
+}
+
+
+# S3からgzファイルを取得してlessで開く
+function szless (){
+    s3cmd get --no-progress $1 - | zcat | less
+}
+
