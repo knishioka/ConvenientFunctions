27e5782e9ad91d10f7f9d2def817a1ae1ace2eb7
Kenichiro Nishioka <nishioka@3idea.jp>
2019-11-25 15:48:05 +0000

    Create docker aliases file

--- shell/alias.zsh
+++ shell/alias.zsh
@@ -1,16 +1,3 @@
-alias dc='docker container'
-alias dca='docker container ls -a'
-alias dce='docker container exec'
-alias dcl='docker container ls'
-alias dcm='docker container rm'
-alias dcp='docker container cp'
-alias di='docker image'
-alias dia='docker image ls -a'
-alias dil='docker image ls'
-alias dn='docker network'
-alias dnl='docker network ls'
-alias dv='docker volume'
-alias dvl='docker volume ls'
 alias get-job-queues="aws batch describe-job-queues | jq '.jobQueues[] | {jobQueueName, jobQueueArn}'"
 alias get-ecr-repositories="aws ecr describe-repositories | jq '.repositories[] | {"repositoryArn": .repositoryArn, \"repositoryUri\": .repositoryUri}'"
 alias gfrh='git fetch && git reset origin/`git rev-parse --abbrev-ref HEAD` --hard'
