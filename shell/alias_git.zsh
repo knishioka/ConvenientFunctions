13f532bbd70078d82f7a17c85b4665946d8eea8d
Kenichiro Nishioka <nishioka@3idea.jp>
2019-11-28 01:02:59 +0900

    Add alias to checkout develop

--- shell/alias_git.zsh
+++ shell/alias_git.zsh
@@ -1,2 +1,3 @@
 alias gfrh='git fetch && git reset origin/`git rev-parse --abbrev-ref HEAD` --hard'
 alias grmmerged="git branch --merged | egrep -v '\\*master' | xargs git branch -d"
+alias gcd='git checkout develop'
