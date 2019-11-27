df0157f89c093d3640553b12bf4798b68f7504b1
Kenichiro Nishioka <nishioka@3idea.jp>
2019-11-27 14:09:47 +0000

    Add alias to remove merged branches

--- shell/alias.zsh
+++ shell/alias.zsh
@@ -1,6 +1,5 @@
 alias get-job-queues="aws batch describe-job-queues | jq '.jobQueues[] | {jobQueueName, jobQueueArn}'"
 alias get-ecr-repositories="aws ecr describe-repositories | jq '.repositories[] | {"repositoryArn": .repositoryArn, \"repositoryUri\": .repositoryUri}'"
-alias gfrh='git fetch && git reset origin/`git rev-parse --abbrev-ref HEAD` --hard'
 alias sls='s3cmd ls'
 alias ta='tmux attach'
 alias tl='tmux ls'
---
+++ shell/git_alias.zsh
@@ -0,0 +1,2 @@
+alias gfrh='git fetch && git reset origin/`git rev-parse --abbrev-ref HEAD` --hard'
+alias grmmerged="git branch --merged | egrep -v '\\*master' | xargs git branch -d"
