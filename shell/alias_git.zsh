ea95af139708834126fb3694807f4400fe234fb6
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2019-11-29 18:24:55 +0900

    Change grm-merged to ignore branch including `master`

--- shell/alias_git.zsh
+++ shell/alias_git.zsh
@@ -1,3 +1,3 @@
 alias gfrh='git fetch && git reset origin/`git rev-parse --abbrev-ref HEAD` --hard'
-alias grm-merged="git branch --merged | egrep -v '\\\\*master' | xargs git branch -d"
+alias grm-merged="git branch --merged | egrep -v 'master' | xargs git branch -d"
 alias gcd='git checkout develop'
