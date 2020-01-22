37d71aa53998f046756f92be37d5194ab018b16a
Kenichiro Nishioka <nishioka@3idea.jp>
2020-01-23 00:27:44 +0900

    Prevent globbing and word splitting

--- shell/functions.zsh
+++ shell/functions.zsh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # kmsの復号化
 function kms-decrypt() {
   aws kms decrypt --ciphertext-blob fileb://<(echo "$1"|base64 -d) | jq .Plaintext --raw-output |base64 -d
@@ -5,42 +7,42 @@ function kms-decrypt() {
 
 # ファイルに日付をつける
 function adddate () {
-    mv $1 `date "+%Y%m%d"`_$1
+    mv "$1" "$(date "+%Y%m%d")_${1}"
 }
 
 # 暗号化しながらファイルに名前をつける
 function ezip () {
-    zip -re `date "+%Y%m%d"`_$1.zip $1
+    zip -re "$(date "+%Y%m%d")_${1}.zip" "$1"
 }
 
 # S3からファイルを取得してそのまま表示
 function sg () {
-    s3cmd get --no-progress $1 -
+    s3cmd get --no-progress "$1" -
 }
 
 # S3からファイルを取得してlessで開く
 function sless() {
-    s3cmd get --no-progress $1 - | less
+    s3cmd get --no-progress "$1" - | less
 }
 
 # S3からgzファイルを取得してzcatで開く
 function szcat () {
-    s3cmd get --no-progress $1 - | zcat
+    s3cmd get --no-progress "$1" - | zcat
 }
 
 # S3からgzファイルを取得してlessで開く
 function szless () {
-    s3cmd get --no-progress $1 - | zcat | less
+    s3cmd get --no-progress "$1" - | zcat | less
 }
 
 # aws lambdaのソースをダウンロード
 function get-lambda () {
-    curl -o $1.zip $(aws lambda get-function --function-name $1 | jq -r '.Code.Location')
+    curl -o "${1}.zip" "$(aws lambda get-function --function-name "$1" | jq -r '.Code.Location')"
 }
 
 # 落ちたjobの情報を取得
 function get-failed-job () {
-    aws batch list-jobs --job-queue $1 --job-status FAILED |
+    aws batch list-jobs --job-queue "$1" --job-status FAILED |
         jq '.jobSummaryList[] | .createdAt |= (./1000 | todate) | .startedAt |= (./1000 | todate) | .stoppedAt |= (./1000 | todate)'
 }
 
@@ -50,6 +52,6 @@ function ec2iid () {
 }
 
 # ecrにあるimageをtagをつけて表示
-func desc-ecr-images () {
-    aws ecr describe-images --repository-name $1 | jq -r '.imageDetails[] | "\(.imageDigest)\t\(.imageTags)"'
+function desc-ecr-images () {
+    aws ecr describe-images --repository-name "$1" | jq -r '.imageDetails[] | "\(.imageDigest)\t\(.imageTags)"'
 }
