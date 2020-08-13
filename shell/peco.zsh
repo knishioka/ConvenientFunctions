001c8788ac7969dd2e1811976febc1aad66572ca
Kenichiro Nishioka <kenichiro.nishioka@bebit.com>
2020-08-13 12:32:00 +0900

    Enable breakline in peco

--- shell/peco.zsh
+++ shell/peco.zsh
@@ -1,5 +1,5 @@
 function peco-select-history() {
-  BUFFER=$(\history -n -r 1 | peco --query "$LBUFFER")
+  BUFFER=$(\history -n -r 1 | peco --query "$LBUFFER" | sed 's/\\n/\'$'\n/g')
   CURSOR=$#BUFFER
   zle clear-screen
 }
